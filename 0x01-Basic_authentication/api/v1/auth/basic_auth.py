c9fa753ec40c69f7dfcc6cff222297bf2118c71e
Franklin Obasi <franklinobasy@gmail.com>
2023-03-08 02:08:24 +0100

    Task 12: Basic - Allow password with :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,7 +65,7 @@ class BasicAuth(Auth):
                 ':' in decoded_base64_authorization_header):
             return None, None
 
-        return tuple(decoded_base64_authorization_header.split(':'))
+        return tuple(decoded_base64_authorization_header.split(':', 1))
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
