47e48e97c9cb257f36089dd9ba6ae36d5610d40a
Franklin Obasi <franklinobasy@gmail.com>
2023-03-08 02:12:30 +0100

    Task 13: Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,8 +18,30 @@ class Auth:
         if path is None or excluded_paths is None or not excluded_paths:
             return True
 
-        if path[-1] != '/':
-            path += '/'
+        # handle * at end of excluded paths
+        if path[-1] == '/':
+            path = path[:-1]
+
+        contains_slash = False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/':
+                excluded_path = excluded_path[:-1]
+                contains_slash = True
+
+            if excluded_path.endswith('*'):
+                idx_after_last_slash = excluded_path.rfind('/') + 1
+                excluded = excluded_path[idx_after_last_slash:-1]
+
+                idx_after_last_slash = path.rfind('/') + 1
+                tmp_path = path[idx_after_last_slash:]
+
+                if excluded in tmp_path:
+                    return False
+
+            if contains_slash:
+                contains_slash = False
+
+        path += '/'
 
         if path in excluded_paths:
             return False
